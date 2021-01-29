afe9cb643c3453b1f718060569d2db7cbeabb893
Gennadij Tolstov <genrih113@yandex.ru>
2021-01-29 23:17:43 +0300

    исправить атрибуты img в попапе картинки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,7 +6,7 @@ function ImagePopup(props) {
         props.onClose()
       }}}>
       <figure className="popup__figure">
-        <img className="popup__place-image" src={`${props.card.link}`} alt={`${props.card.name}`} />
+        <img className="popup__place-image" src={props.card.link} alt={props.card.name} />
         <figcaption className="popup__place-caption">{props.card.name}</figcaption>
         <button className="popup__close popup__close_place-view" type="button" aria-label="Закрыть"
           onClick={props.onClose}>
